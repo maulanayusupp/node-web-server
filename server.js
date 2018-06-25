3fa6069ea3529c6f510a0817a2ca7352f6138e77
Maulana Yusup Abdullah <adeneling@gmail.com>
2018-06-25 16:50:06 +0700

    Rendering template engine with hbs for handlebars and render data

--- server.js
+++ server.js
@@ -1,25 +1,38 @@
 const express = require('express');
 
+/* Express.js template engine plugin for Handlebars */
+const hbs = require('hbs');
+
 const PORT = 3000;
 
 var app = express();
 
-/* middleware */
+/* set template engine */
+app.set('view engine', 'hbs');
+
+/* middleware - static files */
 app.use(express.static(__dirname + '/public'));
 
 /* route */
 app.get('/', (req, res) => {
-	res.send('<h1>Node Web Server!</h1>');
+	// res.send('<h1>Node Web Server!</h1>');
+	var pageName = {
+		name: 'Welcome, Maulana Yusup Abdullah',
+		currentYear: new Date().getFullYear()
+	}
+	res.render('home.hbs', pageName)
 });
 
 app.get('/about', (req, res) => {
-	res.send({
+	var biodata = {
 		name: 'Maulana Yusup Abdullah',
 		likes: [
 			'Adventure',
 			'Breathing'
-		]
-	});
+		],
+		currentYear: new Date().getFullYear()
+	}
+	res.render('about.hbs', biodata)
 });
 
 app.listen(PORT, () => {
